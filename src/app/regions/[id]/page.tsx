94e63c46c2841a9b573ff72d6772a30c7c9bca13
Debashis Roy <goodquote39@hotmail.com>
2023-11-24 17:32:37 +0600

    region page done

---
+++ src/app/regions/[id]/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const regionPage = () => {
+  return (
+    <div className="pt-16">
+      <p>Countries of region</p>
+    </div>
+  );
+};
+
+export default regionPage;
--- src/app/regions/page.tsx
+++ src/app/regions/page.tsx
@@ -1,9 +1,43 @@
-import React from "react";
+/* eslint-disable react/jsx-key */
+/* eslint-disable react-hooks/rules-of-hooks */
+"use client";
+import Link from "next/link";
+import React, { useEffect, useState } from "react";
 
 const regionPage: React.FC = () => {
+  const [regions, setRegions] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await fetch("https://restcountries.com/v3.1/all");
+        const countries = await res.json();
+        const uniqueRegions = Array.from(
+          new Set(countries.map((country: any) => country.region))
+        );
+        setRegions(uniqueRegions.filter(Boolean));
+      } catch (err) {
+        console.error(err);
+      }
+    };
+    fetchData();
+  }, []);
+  // console.log(regions);
   return (
-    <div className="pt-16">
-      <p>Regions</p>
+    <div className="pt-16 text-center">
+      <h3 className="text-xl font-semibold mb-4">List of Regions:</h3>
+      <ul className="w-[95%] md:w-2/5 lg:w-1/5 mx-auto">
+        {regions.map((region) => (
+          <Link
+            key={region}
+            href={`/regions/${region}`}
+          >
+            <li className="border my-1 border-indigo-500 text-indigo-700 text-lg hover:bg-indigo-500 hover:text-white transition-all duration-500">
+              {region}
+            </li>
+          </Link>
+        ))}
+      </ul>
     </div>
   );
 };
